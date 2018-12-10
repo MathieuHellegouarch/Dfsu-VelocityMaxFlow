41b9c1cd2f6dec4b837a9b0e5be2309e9cc6abf8
Mathieu Hellegouarch <mjh@dhigroup.com>
2018-12-10 17:27:06 +0100

    Changed to 64-bit only. Added reference to DHI.Mike.Install.dll.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DHI.Mike.Install;
 
 namespace Compute_Direction_Max
 {
@@ -14,6 +15,7 @@ namespace Compute_Direction_Max
         [STAThread]
         static void Main()
         {
+            MikeImport.SetupLatest(MikeProducts.MikeCore);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
